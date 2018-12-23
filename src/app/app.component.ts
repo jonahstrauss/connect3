5127d1992f6cd8717c925d77cc8bb4bd50340017
Jonah Strauss <yonah.strauss@gmail.com>
2018-12-23 14:35:56 -0500

    Show winner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,4 +38,33 @@ export class AppComponent {
 	  this.whosTurn = this.whosTurn == 'brown' ? 'purple' : 'brown'
   }
   a = this.togglePlayer()
+  winnerDirection(r,c,player,orientation,remaining){
+	  if(player && this.board[r][c] == player){
+		  if(remaining > 0){
+			  let newR = orientation == 'horizontal' ? r : r + 1
+			  let newC = orientation == 'vertical' ? c : c + (orientation == 'diagonalUp' ? -1 : 1)
+			  return this.winnerDirection(newR, newC, player, orientation, remaining - 1)
+		  } else {
+			  return true
+		  }
+	  } else {
+		  return false
+	  }
+  }
+  winner() {
+	  for(var r = 0; r < this.size; r++){
+		  for(var c = 0; c < this.size; c++) {
+			  let player = this.board[r][c]
+			  if(this.winnerOrientation(r,c,player,'horizontal') return player
+			  if(this.winnerOrientation(r,c,player,'vertical') return player
+			  if(this.winnerOrientation(r,c,player, 'diagonal') return player
+			  if(this.winnerOrientation(r,c,player, 'diagonalUp') return player
+		  }			  
+	  }
+	  return 'No winner yet.';
+  }
+  winnerOrientation(r,c,player,orientation){
+	  return this.winnerDirection(r,c,player, orientation, this.size - 2)
+  }
+  
 }
